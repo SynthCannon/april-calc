1a073940edeaaacd929fa4983c61dbcf3d3a29c6
SynthCannon <niksum15@gmail.com>
2018-06-13 11:59:06 +0100

    first commit

---
+++ src/main/java/com/qa/app/App.java
@@ -0,0 +1,10 @@
+package com.qa.app;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello my friends");
+		System.out.println("I actually hate you");
+	}
+
+}
